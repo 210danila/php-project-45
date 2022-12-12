8472cdf63a8ab2e69026a492e7eb638c8a9ad21d
210danila <210danila@gmail.com>
2022-12-12 19:30:43 +0300

    Fix type of gcd in Games/Gcd.php

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -22,7 +22,7 @@ function createPairOfNums()
 {
     $num1 = rand(1, 100);
     $num2 = rand(1, 100);
-    $gcd = findGcdOfTwoNums($num1, $num2);
+    $gcd = (string) findGcdOfTwoNums($num1, $num2);
     return ["{$num1} {$num2}", $gcd];
 }
 
